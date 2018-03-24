ad795184e87e3110d6851724c8835e3fd8a70e84
snmcatrut <freddy90076@gmail.com>
2018-03-25 02:40:42 +0800

    flask server now can fetch DB data for response

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import pony.orm as pny
 from flask import Flask, jsonify, abort, request
+from dateutil import parser
+
+from models import Input1, Input2, Input3, Input4, Input5
 
 app = Flask(__name__)
 
@@ -8,15 +12,31 @@ def not_found(error):
 
 @app.route('/datas', methods=['GET'])
 def get_databases():
-    start = request.args.get('start')
-    end = request.args.get('end')
-    return str(start) + str(end)
+    res = {}
+    for i in range(1, 6):
+        tablename = 'Input' + str(i)
+        start = parser.parse(request.args.get('start'))
+        end = parser.parse(request.args.get('end'))
+        with pny.db_session:
+            k = pny.select((p.timestamp, p.value) for p in globals()[tablename] \
+                            if p.timestamp >= start and p.timestamp <= end)[:]
+        res.update({tablename: [(str(x[0]), x[1]) for x in k]})
+
+    return jsonify(res)
 
 @app.route('/datas/Input<int:database_id>', methods=['GET'])
 def get_database(database_id):
     if database_id > 5 or database_id < 1:
         abort(404)
-    return str(database_id)
+    tablename = 'Input' + str(database_id)
+    start = parser.parse(request.args.get('start'))
+    end = parser.parse(request.args.get('end'))
+    with pny.db_session:
+        k = pny.select((p.timestamp, p.value) for p in globals()[tablename] \
+                        if p.timestamp >= start and p.timestamp <= end)[:]
+    res = {tablename: [(str(x[0]), x[1]) for x in k]}
+
+    return jsonify(res)
 
 if __name__ == '__main__':
     app.run(debug=True)

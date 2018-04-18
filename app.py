b6b0808f6abd2dcfe5be06d0d77168c3b7d9d4a8
Ksoy <ksoyam95@gmail.com>
2018-04-18 15:17:18 +0800

    monitor page prototype

--- app.py
+++ app.py
@@ -1,42 +1,93 @@
+import os
 import pony.orm as pny
-from flask import Flask, jsonify, abort, request
+from flask import (Flask, jsonify, render_template,
+                   request, send_from_directory)
 from dateutil import parser
 
-from models import Input1, Input2, Input3, Input4, Input5
+import models
 
 app = Flask(__name__)
+default_limit = 100
+
 
 @app.errorhandler(404)
 def not_found(error):
     return 'invalid database', 404
 
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('dashboard.html')
+
+
+@app.route('/history', methods=['GET'])
+def history():
+    return render_template('history.html')
+
+
 @app.route('/datas', methods=['GET'])
 def get_databases():
     res = {}
-    for i in range(1, 6):
-        tablename = 'Input' + str(i)
-        start = parser.parse(request.args.get('start'))
-        end = parser.parse(request.args.get('end'))
+
+    start = request.args.get('start')
+    end = request.args.get('end')
+    limit = int(request.args.get('limit', default_limit))
+
+    if start and end:
+        start = parser.parse(start)
+        end = parser.parse(end)
+
+    for tablename in models.database.entities:
+        table = models.database.entities.get(tablename)
         with pny.db_session:
-            k = pny.select((p.timestamp, p.value) for p in globals()[tablename] \
-                            if p.timestamp >= start and p.timestamp <= end)[:]
-        res.update({tablename: [(str(x[0]), x[1]) for x in k]})
+            if start and end:
+                query = table.select(lambda row: row.timestamp >= start and row.timestamp <= end)
+            else:
+                query = table.select()
+
+            query = query.order_by(pny.desc(table.timestamp)).limit(limit)
+
+        res.update({tablename: [(str(record.timestamp), record.value) for record in query]})
 
     return jsonify(res)
 
-@app.route('/datas/Input<int:database_id>', methods=['GET'])
-def get_database(database_id):
-    if database_id > 5 or database_id < 1:
-        abort(404)
-    tablename = 'Input' + str(database_id)
-    start = parser.parse(request.args.get('start'))
-    end = parser.parse(request.args.get('end'))
+
+@app.route('/datas/<string:tablename>', methods=['GET'])
+def get_database(tablename):
+    if not hasattr(models, tablename):
+        return "table not find"
+    table = getattr(models, tablename)
+
+    start = request.args.get('start')
+    end = request.args.get('end')
+    limit = int(request.args.get('limit', default_limit))
+
+    if start and end:
+        start = parser.parse(start)
+        end = parser.parse(end)
+
     with pny.db_session:
-        k = pny.select((p.timestamp, p.value) for p in globals()[tablename] \
-                        if p.timestamp >= start and p.timestamp <= end)[:]
-    res = {tablename: [(str(x[0]), x[1]) for x in k]}
+        if start and end:
+            query = table.select(lambda row: row.timestamp >= start and row.timestamp <= end)
+        else:
+            query = table.select()
+
+        query = query.order_by(pny.desc(table.timestamp)).limit(limit)
+
+    res = {tablename: [(str(record.timestamp), record.value) for record in query]}
 
     return jsonify(res)
+
 
 def data_server():
-    app.run(debug=False)
+    app.run(debug=False, threaded=True)
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)

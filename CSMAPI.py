f7093034b662232180ee9f830d445295110d5ba0
Ksoy <ksoyam95@gmail.com>
2018-04-18 16:17:27 +0800

    update dan/csmapi

--- CSMAPI.py
+++ CSMAPI.py
@@ -1,59 +1,59 @@
 import requests
 
+ENDPOINT = None
+TIMEOUT=10
+IoTtalk = requests.Session()
+#print(IoTtalk)
+
 class CSMError(Exception):
     pass
 
-class CSMAPI():
-
-    IoTtalk = requests.Session()
-
-    def __init__(self, ENDPOINT=None, TIMEOUT=10):
-        self.ENDPOINT = ENDPOINT
-        self.TIMEOUT = TIMEOUT    
-
-    def register(self, mac_addr, profile):
-        r = self.IoTtalk.post(
-            self.ENDPOINT + '/' + mac_addr,
-            json={'profile': profile}, timeout=self.TIMEOUT
-        )
-        if r.status_code != 200: raise CSMError(r.text)
-        return True
+def register(mac_addr, profile, UsingSession=IoTtalk):
+    r = UsingSession.post(
+        ENDPOINT + '/' + mac_addr,
+        json={'profile': profile}, timeout=TIMEOUT
+    )
+    if r.status_code != 200: raise CSMError(r.text)
+    return True
 
 
-    def deregister(self, mac_addr):
-        r = self.IoTtalk.delete(self.ENDPOINT + '/' + mac_addr)
-        if r.status_code != 200: raise CSMError(r.text)
-        return True
+def deregister(mac_addr, UsingSession=IoTtalk):
+    r = UsingSession.delete(ENDPOINT + '/' + mac_addr)
+    if r.status_code != 200: raise CSMError(r.text)
+    return True
 
 
-    def push(self, mac_addr, df_name, data):
-        r = self.IoTtalk.put(
-            self.ENDPOINT + '/' + mac_addr + '/' + df_name,
-            json={'data': data}, timeout=self.TIMEOUT
-        )
-        if r.status_code != 200: raise CSMError(r.text)
-        return True
+def push(mac_addr, df_name, data, UsingSession=IoTtalk):
+    r = UsingSession.put(
+        ENDPOINT + '/' + mac_addr + '/' + df_name,
+        json={'data': data}, timeout=TIMEOUT
+    )
+    if r.status_code != 200: raise CSMError(r.text)
+    return True
 
 
-    def pull(self, mac_addr, df_name):
-        r = self.IoTtalk.get(self.ENDPOINT + '/' + mac_addr + '/' + df_name, timeout=self.TIMEOUT)
-        if r.status_code != 200: raise CSMError(r.text)
-        return r.json()['samples']
+def pull(mac_addr, df_name, UsingSession=IoTtalk):
+    #print(UsingSession)
+    r = UsingSession.get(ENDPOINT + '/' + mac_addr + '/' + df_name, timeout=TIMEOUT)
+    if r.status_code != 200: raise CSMError(r.text)
+    return r.json()['samples']
 
 
-    def get_alias(self, mac_addr, df_name):
-        r = self.IoTtalk.get(self.ENDPOINT + '/get_alias/' + mac_addr + '/' + df_name, timeout=self.TIMEOUT)
-        if r.status_code != 200: raise CSMError(r.text)
-        return r.json()['alias_name']
+def get_alias(mac_addr, df_name, UsingSession=IoTtalk):
+    r = UsingSession.get(ENDPOINT + '/get_alias/' + mac_addr + '/' + df_name, timeout=TIMEOUT)
+    if r.status_code != 200: raise CSMError(r.text)
+    return r.json()['alias_name']
 
+	
+def set_alias(mac_addr, df_name, s, UsingSession=IoTtalk):
+    r = UsingSession.get(ENDPOINT + '/set_alias/' + mac_addr + '/' + df_name + '/alias?name=' + s, timeout=TIMEOUT)
+    if r.status_code != 200: raise CSMError(r.text)
+    return True
 
-    def set_alias(self, mac_addr, df_name, s):
-        r = self.IoTtalk.get(self.ENDPOINT + '/set_alias/' + mac_addr + '/' + df_name + '/alias?name=' + s, timeout=self.TIMEOUT)
-        if r.status_code != 200: raise CSMError(r.text)
-        return True
+	
+def tree(UsingSession=IoTtalk):
+    r = UsingSession.get(ENDPOINT + '/tree')
+    if r.status_code != 200: raise CSMError(r.text)
+    return r.json()
 
-
-    def tree(self):
-        r = self.IoTtalk.get(self.ENDPOINT + '/tree')
-        if r.status_code != 200: raise CSMError(r.text)
-        return r.json()
+	

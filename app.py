8d81e9aa2c7ce39baa1736dc3dfe0006e8120400
IoTtalk <iottalk.service@gmail.com>
2018-04-19 10:28:02 +0800

    update

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ def get_database(tablename):
 
 
 def data_server():
-    app.run(debug=True, threaded=True)
+    app.run(host='0.0.0.0', debug=True, threaded=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    app.run('0', debug=True, threaded=True)
